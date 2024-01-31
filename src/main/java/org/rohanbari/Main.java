21a700e7e2e0b3fb6a5625468feb926ad9dffd1c
rohanbari <rohanbari4@gmail.com>
2024-01-31 10:55:04 +0530

    Solved TwoSum_1 in the most optimized way

--- src/main/java/org/rohanbari/Main.java
+++ src/main/java/org/rohanbari/Main.java
@@ -4,10 +4,6 @@
 
 package org.rohanbari;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 /**
  * This file is dedicated to the problem I am currently working on.
  */
@@ -19,33 +15,6 @@ public class Main {
      * @param args System arguments
      */
     public static void main(String[] args) {
-        System.out.println("Working on: 3Sum");
-
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        System.out.println(threeSum(nums));
-    }
-
-    private static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                for (int k = 0; k < nums.length; k++) {
-                    if (i != j && j != k && k != i) {
-                        if (nums[i] + nums[j] + nums[k] == 0) {
-                            List<Integer> subList = new ArrayList<>();
-
-                            subList.add(nums[i]);
-                            subList.add(nums[j]);
-                            subList.add(nums[k]);
-
-                            list.add(subList);
-                        }
-                    }
-                }
-            }
-        }
-
-        return new ArrayList<>(new HashSet<>(list));
+        System.out.println("Most recently solved: 1");
     }
 }
---
+++ src/main/java/org/rohanbari/code/TwoSum_1.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Rohan Bari <rohanbari@outlook.com>
+ */
+
+package org.rohanbari.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_1 {
+
+    /**
+     * This function returns an array of an integer pair containing the indices
+     * of array elements that sums up to the target value.
+     *
+     * @param nums   The array to be tested on
+     * @param target The desired result
+     * @return The array of two-indices pairs
+     */
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+
+        return nums;
+    }
+}
