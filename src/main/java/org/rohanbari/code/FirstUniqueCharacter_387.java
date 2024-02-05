15ae0fdfe8d89997c4bdbf9b3eacbb2620cc411c
Rohan Bari <rohanbari4@gmail.com>
2024-02-05 09:43:26 +0530

    Solved problem FirstUniqueCharacter_387

---
+++ src/main/java/org/rohanbari/code/FirstUniqueCharacter_387.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Rohan Bari <rohanbari@outlook.com>
+ */
+
+package org.rohanbari.code;
+
+class FirstUniqueCharacter_387 {
+
+    public static int firstUniqChar(String s) {
+        for (var ch : s.toCharArray()) {
+            if (s.indexOf(ch) == s.lastIndexOf(ch)) {
+                return s.indexOf(ch);
+            }
+        }
+
+        return -1;
+    }
+}
