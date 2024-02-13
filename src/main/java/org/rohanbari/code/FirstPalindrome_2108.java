8a64d68ecf62d2d0a8cd48d282d5c5d31f002b9c
Rohan Bari <rohanbari4@gmail.com>
2024-02-13 06:36:14 +0530

    Solved problem FirstPalindrome_2108

---
+++ src/main/java/org/rohanbari/code/FirstPalindrome_2108.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Rohan Bari <rohanbari@outlook.com>
+ */
+
+package org.rohanbari.code;
+
+class FirstPalindrome_2108 {
+
+    public static String firstPalindrome(String[] words) {
+        for (String word : words)
+            if (word.contentEquals(new StringBuilder(word).reverse()))
+                return word;
+
+        return "";
+    }
+}
