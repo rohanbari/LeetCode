7fc31c1f830301377ff5fad53560c1861ebeba13
Rohan Bari <rohanbari4@gmail.com>
2024-02-13 06:27:48 +0530

    Solved problem MajorityElement_169

---
+++ src/main/java/org/rohanbari/code/MajorityElement_169.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Rohan Bari <rohanbari@outlook.com>
+ */
+
+package org.rohanbari.code;
+
+import java.util.Arrays;
+
+class MajorityElement_169 {
+    public static int majorityElement1(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0)
+                candidate = num;
+
+            if (candidate == count)
+                count++;
+            else
+                count--;
+        }
+
+        return candidate;
+    }
+
+    public static int majorityElement2(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
