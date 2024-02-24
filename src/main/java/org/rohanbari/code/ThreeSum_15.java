06da354f4f42b6bf84b8e3fd2a57ab91c72ac5a7
Rohan Bari <rohanbari4@gmail.com>
2024-02-24 06:38:00 +0530

    Solved problem 15
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/main/java/org/rohanbari/code/ThreeSum_15.java
@@ -0,0 +1,65 @@
+/*
+ Copyright 2024 Rohan Bari <rohanbari@outlook.com>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.rohanbari.code;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum_15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int length = nums.length;
+
+        if (length == 0)
+            return new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum < 0)
+                    left++;
+                else if (sum > 0)
+                    right--;
+                else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
