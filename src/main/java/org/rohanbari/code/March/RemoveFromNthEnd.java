15498552a0a34a94b23f4a3ad10f7ae2181dc6c5
Rohan Bari <rohanbari4@gmail.com>
2024-03-03 09:55:13 +0530

    Solved problem 19
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/main/java/org/rohanbari/code/March/RemoveFromNthEnd.java
@@ -0,0 +1,65 @@
+/*
+ Copyright 2024 Rohan Bari <rohanbari@outlook.com>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.rohanbari.code.March;
+
+/**
+ * 19. Remove Nth Element from End
+ */
+public class RemoveFromNthEnd {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Remove the (size - n)th element of the linked-list.
+     * 
+     * @param head Head of the list
+     * @param n    nth position from the end
+     * @return Updated result
+     */
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++)
+            fast = fast.next;
+
+        if (fast == null)
+            return head.next;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
