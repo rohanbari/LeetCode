d9d39bd649f271bff9f1579aed06085ade74468b
Rohan Bari <rohanbari4@gmail.com>
2024-05-05 09:20:40 +0530

    Solved problem 239, reached 100 days streak milestone!
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

--- src/main/java/org/rohanbari/code/Main.java
+++ src/main/java/org/rohanbari/code/Main.java
@@ -14,19 +14,11 @@
  limitations under the License.
  */
 
-package org.rohanbari;
+package org.rohanbari.code;
 
-/**
- * This file is dedicated to the problem I am currently working on.
- */
 public class Main {
 
-    /**
-     * The main method.
-     * 
-     * @param args System arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Latest solved: 1137th problem.");
+        System.out.println("Latest solved: 237");
     }
 }
